8b1133576c5f036dcba57c85dce5cb55c9ea2176
James Barfield <barfieldjames@gmail.com>
2018-10-12 08:41:05 -0400

    Initial commit

---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ ! -f .env ]
+then
+
+  echo "You must include a .env file"
+
+else
+
+  export $(cat .env | xargs)
+
+  docker run -d -e "CONTROLLER_HOST=$CONTROLLER_HOST" -e "CONTROLLER_PORT=$CONTROLLER_PORT" -e "CONTROLLER_SSL_ENABLED=$CONTROLLER_SSL_ENABLED" \
+		-e "APPLICATION_NAME=$APPLICATION_NAME" -e "ACCOUNT_NAME=$ACCOUNT_NAME" -e "ACCOUNT_ACCESS_KEY=$ACCOUNT_ACCESS_KEY" \
+		-e "EVENT_ENDPOINT=$EVENT_ENDPOINT" -e "FULL_ACCOUNT_NAME=$FULL_ACCOUNT_NAME" \
+		--expose "9090" --network=$NETWORK_NAME --hostname machine-agent \
+		--volume "/var/run/docker.sock:/var/run/docker.sock" --name machine-agent jdbarfield/dynamic-attach-ma
+
+fi
