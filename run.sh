f2cdee423407b31aed094d04dda730045246dd9e
James Barfield <barfieldjames@gmail.com>
2018-10-17 16:01:24 -0400

    c

--- run.sh
+++ run.sh
@@ -11,8 +11,8 @@ else
 
   docker run -d -e "CONTROLLER_HOST=$CONTROLLER_HOST" -e "CONTROLLER_PORT=$CONTROLLER_PORT" -e "CONTROLLER_SSL_ENABLED=$CONTROLLER_SSL_ENABLED" \
 		-e "APPLICATION_NAME=$APPLICATION_NAME" -e "ACCOUNT_NAME=$ACCOUNT_NAME" -e "ACCOUNT_ACCESS_KEY=$ACCOUNT_ACCESS_KEY" \
-		-e "EVENT_ENDPOINT=$EVENT_ENDPOINT" -e "FULL_ACCOUNT_NAME=$FULL_ACCOUNT_NAME" \
+		-e "EVENT_ENDPOINT=$EVENT_ENDPOINT" -e "FULL_ACCOUNT_NAME=$FULL_ACCOUNT_NAME" -e "TIER_NAME_FROM=$TIER_NAME_FROM" -e "TIER_NAME_PARAM=$TIER_NAME_PARAM" \
 		--expose "9090" --network=$NETWORK_NAME --hostname machine-agent \
-		--volume "/var/run/docker.sock:/var/run/docker.sock" --name machine-agent jdbarfield/dynamic-attach-ma:4.5.2
+		--volume "/var/run/docker.sock:/var/run/docker.sock" --name machine-agent machine-agent
 
 fi
