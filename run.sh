002adab5c25e025e406f87d08f37dba5ec6a8e0a
James Barfield <barfieldjames@gmail.com>
2018-10-18 08:55:58 -0400

    c

--- run.sh
+++ run.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 
-if [ ! -f .env ]
+if [ ! -f ma.env ]
 then
 
-  echo "You must include a .env file"
+  echo "You must include an ma.env file"
 
 else
 
-  export $(cat .env | xargs)
+  export $(cat ma.env | xargs)
 
-  docker run -d -e "CONTROLLER_HOST=$CONTROLLER_HOST" -e "CONTROLLER_PORT=$CONTROLLER_PORT" -e "CONTROLLER_SSL_ENABLED=$CONTROLLER_SSL_ENABLED" \
+  if [[ "x$FULL_ACCOUNT_NAME" != "x" && "x$EVENT_ENDPOINT" != "x" && "x$NETWORK_NAME" != "x" ]]; then
+
+    # Enable analytics
+    docker run -d -e "CONTROLLER_HOST=$CONTROLLER_HOST" -e "CONTROLLER_PORT=$CONTROLLER_PORT" -e "CONTROLLER_SSL_ENABLED=$CONTROLLER_SSL_ENABLED" \
 		-e "APPLICATION_NAME=$APPLICATION_NAME" -e "ACCOUNT_NAME=$ACCOUNT_NAME" -e "ACCOUNT_ACCESS_KEY=$ACCOUNT_ACCESS_KEY" \
-		-e "EVENT_ENDPOINT=$EVENT_ENDPOINT" -e "FULL_ACCOUNT_NAME=$FULL_ACCOUNT_NAME" -e "TIER_NAME_FROM=$TIER_NAME_FROM" -e "TIER_NAME_PARAM=$TIER_NAME_PARAM" \
-		--expose "9090" --network=$NETWORK_NAME --hostname machine-agent \
-		--volume "/var/run/docker.sock:/var/run/docker.sock" --name machine-agent machine-agent
+		-e "EVENT_ENDPOINT=$EVENT_ENDPOINT" -e "FULL_ACCOUNT_NAME=$FULL_ACCOUNT_NAME" --network=$NETWORK_NAME \
+		-e "TIER_NAME_FROM=$TIER_NAME_FROM" -e "TIER_NAME_PARAM=$TIER_NAME_PARAM" -e "ENABLE_ANALYTICS=1" \
+		--expose "9090" --hostname machine-agent --volume "/var/run/docker.sock:/var/run/docker.sock" --name machine-agent machine-agent
 
+  else
+
+    # Disable analytics
+    docker run -d -e "CONTROLLER_HOST=$CONTROLLER_HOST" -e "CONTROLLER_PORT=$CONTROLLER_PORT" -e "CONTROLLER_SSL_ENABLED=$CONTROLLER_SSL_ENABLED" \
+		-e "APPLICATION_NAME=$APPLICATION_NAME" -e "ACCOUNT_NAME=$ACCOUNT_NAME" -e "ACCOUNT_ACCESS_KEY=$ACCOUNT_ACCESS_KEY" \
+		-e "TIER_NAME_FROM=$TIER_NAME_FROM" -e "TIER_NAME_PARAM=$TIER_NAME_PARAM" -e "ENABLE_ANALYTICS=0" \
+		--expose "9090" --hostname machine-agent --volume "/var/run/docker.sock:/var/run/docker.sock" --name machine-agent machine-agent
+
+  fi
+	
 fi
