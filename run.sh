509a0361e5a537ab3c586ab56991f1c4b8169b6e
James Barfield <barfieldjames@gmail.com>
2018-12-03 14:28:52 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -29,7 +29,7 @@ else
         -e "APPLICATION_NAME=$APPLICATION_NAME" -e "ACCOUNT_NAME=$ACCOUNT_NAME" -e "ACCOUNT_ACCESS_KEY=$ACCOUNT_ACCESS_KEY" \
         -e "EVENT_ENDPOINT=$EVENT_ENDPOINT" -e "FULL_ACCOUNT_NAME=$FULL_ACCOUNT_NAME" -e "ENABLE_NODEJS_INJECTION=$ENABLE_NODEJS_INJECTION" \
         -e "TIER_NAME_FROM=$TIER_NAME_FROM" -e "TIER_NAME_PARAM=$TIER_NAME_PARAM" -e "ENABLE_ANALYTICS=1" \
-        --expose "9090" --hostname machine-agent --volume "/var/run/docker.sock:/var/run/docker.sock" --name machine-agent machine-agent
+        --expose "9090" --hostname machine-agent --volume "/var/run/docker.sock:/var/run/docker.sock" --name machine-agent jdbarfield/dynamic-attach-ma
 
     else
 
@@ -37,7 +37,7 @@ else
       docker run -d -e "CONTROLLER_HOST=$CONTROLLER_HOST" -e "CONTROLLER_PORT=$CONTROLLER_PORT" -e "CONTROLLER_SSL_ENABLED=$CONTROLLER_SSL_ENABLED" \
   		-e "APPLICATION_NAME=$APPLICATION_NAME" -e "ACCOUNT_NAME=$ACCOUNT_NAME" -e "ACCOUNT_ACCESS_KEY=$ACCOUNT_ACCESS_KEY" \
 		  -e "TIER_NAME_FROM=$TIER_NAME_FROM" -e "TIER_NAME_PARAM=$TIER_NAME_PARAM" -e "ENABLE_ANALYTICS=0" -e "ENABLE_NODEJS_INJECTION=$ENABLE_NODEJS_INJECTION" \
-  		--expose "9090" --volume "/var/run/docker.sock:/var/run/docker.sock" --name machine-agent machine-agent
+  		--expose "9090" --volume "/var/run/docker.sock:/var/run/docker.sock" --name machine-agent jdbarfield/dynamic-attach-ma
 
     fi
 
