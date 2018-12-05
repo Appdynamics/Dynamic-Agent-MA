718d7232f23bce8d033c5fc227e88ad2fab8bb9c
James Barfield <barfieldjames@gmail.com>
2018-12-05 15:31:24 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -21,9 +21,9 @@ else
     docker run -d -e "CONTROLLER_HOST=$CONTROLLER_HOST" -e "CONTROLLER_PORT=$CONTROLLER_PORT" -e "CONTROLLER_SSL_ENABLED=$CONTROLLER_SSL_ENABLED" \
       -e "APPLICATION_NAME=$APPLICATION_NAME" -e "ACCOUNT_NAME=$ACCOUNT_NAME" -e "ACCOUNT_ACCESS_KEY=$ACCOUNT_ACCESS_KEY" \
       -e "EVENT_ENDPOINT=$EVENT_ENDPOINT" -e "FULL_ACCOUNT_NAME=$FULL_ACCOUNT_NAME" -e "ENABLE_NODEJS_INJECTION=$ENABLE_NODEJS_INJECTION" \
-      -e "TIER_NAME_FROM=$TIER_NAME_FROM" -e "TIER_NAME_PARAM=$TIER_NAME_PARAM" --expose "9090"  --volume "/var/run/docker.sock:/var/run/docker.sock" \
+      -e "TIER_NAME_FROM=$TIER_NAME_FROM" -e "TIER_NAME_PARAM=$TIER_NAME_PARAM" --expose "9090" --volume=/:/hostroot:ro --volume "/var/run/docker.sock:/var/run/docker.sock" \
       --hostname machine-agent --name machine-agent machine-agent
 
   fi
-	
+
 fi
