36e8d7954ef2e0104aa5e79662557bb40bc962f0
James Barfield <barfieldjames@gmail.com>
2018-12-05 07:20:19 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,6 +7,9 @@ then
 
 else
 
+  docker pull jdbarfield/dynamic-attach-ma
+  docker tag jdbarfield/dynamic-attach-ma machine-agent
+  
   export $(cat controller.env | xargs)
 
   if [[ "x$CONTROLLER_HOST" == "x" || "x$CONTROLLER_PORT" == "x" || "x$CONTROLLER_SSL_ENABLED" == "x" || "x$APPLICATION_NAME" == "x" || "x$ACCOUNT_ACCESS_KEY" == "x" ]]; then
@@ -22,7 +25,7 @@ else
         -e "APPLICATION_NAME=$APPLICATION_NAME" -e "ACCOUNT_NAME=$ACCOUNT_NAME" -e "ACCOUNT_ACCESS_KEY=$ACCOUNT_ACCESS_KEY" \
         -e "EVENT_ENDPOINT=$EVENT_ENDPOINT" -e "FULL_ACCOUNT_NAME=$FULL_ACCOUNT_NAME" -e "ENABLE_NODEJS_INJECTION=$ENABLE_NODEJS_INJECTION" \
         -e "TIER_NAME_FROM=$TIER_NAME_FROM" -e "TIER_NAME_PARAM=$TIER_NAME_PARAM" -e "ENABLE_ANALYTICS=1" \
-        --expose "9090" --hostname machine-agent --volume "/var/run/docker.sock:/var/run/docker.sock" --name machine-agent jdbarfield/dynamic-attach-ma
+        --expose "9090" --hostname machine-agent --volume "/var/run/docker.sock:/var/run/docker.sock" --name machine-agent machine-agent
 
     else
 
@@ -30,7 +33,7 @@ else
       docker run -d -e "CONTROLLER_HOST=$CONTROLLER_HOST" -e "CONTROLLER_PORT=$CONTROLLER_PORT" -e "CONTROLLER_SSL_ENABLED=$CONTROLLER_SSL_ENABLED" \
   		-e "APPLICATION_NAME=$APPLICATION_NAME" -e "ACCOUNT_NAME=$ACCOUNT_NAME" -e "ACCOUNT_ACCESS_KEY=$ACCOUNT_ACCESS_KEY" \
 		  -e "TIER_NAME_FROM=$TIER_NAME_FROM" -e "TIER_NAME_PARAM=$TIER_NAME_PARAM" -e "ENABLE_ANALYTICS=0" -e "ENABLE_NODEJS_INJECTION=$ENABLE_NODEJS_INJECTION" \
-  		--expose "9090" --volume "/var/run/docker.sock:/var/run/docker.sock" --name machine-agent jdbarfield/dynamic-attach-ma
+  		--expose "9090" --volume "/var/run/docker.sock:/var/run/docker.sock" --name machine-agent machine-agent
 
     fi
 
